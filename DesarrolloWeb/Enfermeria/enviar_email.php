c068f5924049fd53191663ec2f7c818d34b09fe6
fagarcialo <fabio_andres_g@hotmail.com>
2018-02-14 17:26:32 -0500

    .

---
+++ DesarrolloWeb/Enfermeria/enviar_email.php
@@ -0,0 +1,15 @@
+<html>
+  <body>
+	<h1>Mensaje enviado con exito</h1>
+	<?php
+	$nombre = $_POST['nombre'];
+	$telefono = $_POST['telefono'];
+	$email = $_POST['email'];
+	$ciudad = $_POST['ciudad'];
+	$mensaje = 'El señor(a) '.nombre.', esta interesado en nuestros servicios. Por favor comunicarse al '.$telefono.' o al correo '.$email;
+	echo 'hola';
+	//mail('fagarcia@itc.com.co', 'Pruebas Contactenos pagina',utf8_decode($mensaje));
+	?>
+  </body>
+
+</html>
